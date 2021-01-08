5892e7dcae2714fd1ede5954fefbaf3fc7a8afcf
Michael-IRL <iszekeell@gmail.com>
2021-01-08 21:11:59 +0000

    update app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+
+import AddToDo from './containers/AddToDo';
+import ToDoListContainer from './containers/ToDoListContainer';
 
 function App() {
 	return (
 		<Container>
 			<Row className='row'>
 				<Col xs={12}>
-					<h1>My New React Bootstrap SPA</h1>
-					<Button>Look, I'm a button!</Button>
+					<h1>To Do List</h1>
+					<AddToDo />
+					<ToDoListContainer />
 				</Col>
 			</Row>
 		</Container>
