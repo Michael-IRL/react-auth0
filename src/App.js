4ee19b81adac606dad54708300119566efc709e3
Michael-IRL <iszekeell@gmail.com>
2021-01-09 12:53:17 +0000

    Add navigation and routes to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import { Route } from 'react-router-dom';
 
 import AddToDo from './containers/AddToDo';
 import ToDoListContainer from './containers/ToDoListContainer';
+import Navigation from './components/Navigation';
 
 function App() {
 	return (
@@ -11,8 +13,9 @@ function App() {
 			<Row className='row'>
 				<Col xs={12}>
 					<h1>To Do List</h1>
-					<AddToDo />
-					<ToDoListContainer />
+					<Navigation />
+					<Route exact path='/' component={ToDoListContainer} />
+					<Route exact path='/new-item' component={AddToDo} />
 				</Col>
 			</Row>
 		</Container>
