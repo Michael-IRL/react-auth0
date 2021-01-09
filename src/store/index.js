6f98d9b93b71add0a67382c821c0dcafb9a08ec4
Michael-IRL <iszekeell@gmail.com>
2021-01-09 10:12:12 +0000

    add saga middleware to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,19 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import toDoApp from '../reducers';
+import rootSaga from '../sagas';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export const store = createStore(toDoApp);
+const sagaMiddleware = createSagaMiddleware();
+
+let composeEnhancers = compose;
+
+if (process.env.NODE_ENV === 'development') {
+	composeEnhancers = composeWithDevTools({ trace: true, traceLimit: 25 });
+}
+
+const store = createStore(toDoApp, composeEnhancers(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
